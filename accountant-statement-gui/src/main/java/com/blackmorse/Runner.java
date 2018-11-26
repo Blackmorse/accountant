6ed60e6475d6f58c702ea9d098d78b45f3c8feaa
Кочемировский Андрей Владимирович <a.kochemirovskij@cft.ru>
2018-11-26 14:48:34 +0700

    #5 Add configuration for statements Locations

--- accountant-statement-gui/src/main/java/com/blackmorse/Runner.java
+++ accountant-statement-gui/src/main/java/com/blackmorse/Runner.java
@@ -1,23 +1,42 @@
 package com.blackmorse;
 
+import com.blackmorse.configuration.Configuration;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 /**
  * Hello world!
  *
  */
-public class Runner extends Application
-{
+public class Runner extends Application {
+    private static String[] arguments;
+
     public static void main( String[] args ) {
+        arguments = args;
         launch(args);
     }
 
     @Override
     public void start(Stage stage) throws Exception {
+
+        Configuration configuration = null;
+        try (InputStream in = Files.newInputStream(Paths.get(arguments[0]))){
+            Yaml yaml = new Yaml();
+            configuration = yaml.loadAs(in, Configuration.class);
+        } catch (Exception e) {
+            System.out.println("Error while loading configuration");
+            e.printStackTrace();
+            throw new Exception();
+        }
+
         String fxmlFile = "/fxml/main.fxml";
         FXMLLoader loader = new FXMLLoader();
         Parent root = loader.load(getClass().getResourceAsStream(fxmlFile));
---
+++ accountant-statement-gui/src/main/java/com/blackmorse/configuration/Configuration.java
@@ -0,0 +1,10 @@
+package com.blackmorse.configuration;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class Configuration {
+    private List<String> statementPaths;
+}
